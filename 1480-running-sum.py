587ac2d414a4b4619d8340690cfc23d7f04d5479
Hyunjun Cho <94371844+Hyunjun999@users.noreply.github.com>
2023-02-16 17:25:30 +0900

    1480 test cases are added

--- 1480-running-sum.py
+++ 1480-running-sum.py
@@ -1,3 +1,8 @@
 class Solution:
     def runningSum(self, nums: list[int]) -> list[int]:
         return [sum(nums[:i]) for i in range(1, len(nums) + 1)]
+    
+s = Solution
+assert(s.runningSum(s, [1,2,3,4]) == [1,3,6,10])
+assert(s.runningSum(s, [1,1,1,1,1]) == [1,2,3,4,5])
+assert(s.runningSum(s, [3,1,2,10,1]) == [3,4,6,16,17])
